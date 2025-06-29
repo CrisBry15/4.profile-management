6677b249adfea7b6de13d9fef362746a38db372e
BryCris15 <bcmalla@uce.edu.ec>
2025-06-29 17:25:24 -0500

    add change models

--- app/models.py
+++ app/models.py
@@ -1,15 +1,15 @@
 import mysql.connector
-from config import Config
+from config import DBConnections
 
 # Obtener datos de perfil del cliente
 def get_client_profile(user_id):
     try:
         conn = mysql.connector.connect(
-            host=Config.CLIENT_DB["host"],
-            port=Config.CLIENT_DB["port"],
-            user=Config.CLIENT_DB["user"],
-            password=Config.CLIENT_DB["password"],
-            database=Config.CLIENT_DB["database"]
+            host=DBConnections.CLIENT_DB["host"],
+            port=DBConnections.CLIENT_DB["port"],
+            user=DBConnections.CLIENT_DB["user"],
+            password=DBConnections.CLIENT_DB["password"],
+            database=DBConnections.CLIENT_DB["database"]
         )
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT id, name, email, phone, status FROM users WHERE id = %s", (user_id,))
@@ -25,11 +25,11 @@ def get_client_profile(user_id):
 def get_organizer_profile(organizer_id):
     try:
         conn = mysql.connector.connect(
-            host=Config.ORGANIZER_DB["host"],
-            port=Config.ORGANIZER_DB["port"],
-            user=Config.ORGANIZER_DB["user"],
-            password=Config.ORGANIZER_DB["password"],
-            database=Config.ORGANIZER_DB["database"]
+            host=DBConnections.ORGANIZER_DB["host"],
+            port=DBConnections.ORGANIZER_DB["port"],
+            user=DBConnections.ORGANIZER_DB["user"],
+            password=DBConnections.ORGANIZER_DB["password"],
+            database=DBConnections.ORGANIZER_DB["database"]
         )
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT id, name, email, phone, status FROM organizers WHERE id = %s", (organizer_id,))
